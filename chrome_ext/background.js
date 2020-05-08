17a16d26743a3693cfcdc8df93d596d2f82439fe
Ivan Ivanov <30338+ivancho@users.noreply.github.com>
2020-05-07 21:36:58 -0400

    Add scrape mode to extension

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -1,24 +1,77 @@
-// Global toggle for the icon
-let ENABLED = true
+let MAIN_HOST = 'tv.nrk.no',
+    SUBS_URLS = '*://undertekst.nrk.no/*.vtt'
 
-chrome.browserAction.onClicked.addListener((tab) => {
-  ENABLED = !ENABLED
-  if (ENABLED)
-    chrome.browserAction.setIcon({path: "icons/on.png", tabId:tab.id})
-  else
-    chrome.browserAction.setIcon({path: "icons/off.png", tabId:tab.id})
-})
+let Downloads = new Set()
 
-// And the intercept for the subtitles requests
-chrome.webRequest.onBeforeRequest.addListener(
-  (req) => {
-    if (req.url.endsWith('.vtt') && ENABLED) {
+let Mode = {
+  Frøya: {
+    title: 'Frøya: English subtitles',
+    icon: 'icons/Frøya.png',
+    handle (req) {
       let filename = 'subs/' + req.url.split(/(\\|\/)/g).pop()
       return {redirectUrl: chrome.runtime.getURL(filename)}
     }
   },
-  {
-    urls: ["*://undertekst.nrk.no/*"]
+  Orm: {
+    title: 'Orm: Norse subtitles',
+    icon: 'icons/Orm.png',
+    handle () {}
   },
-  ["blocking"]
+  Varg: {
+    title: 'Varg: Norse subtitles, downloads them',
+    icon: 'icons/Varg.png',
+    handle (req) {
+      if (!Downloads.has(req.url)) {
+        Downloads.add(req.url)
+        chrome.downloads.download({url: req.url})
+      }
+    }
+  }
+}
+
+
+let State = {'Mode': Object.keys(Mode)}
+let activeMode = () => Mode[State.Mode[0]]
+let rotateMode = () => State.Mode.push(State.Mode.shift())
+
+
+// Update icon and tooltip to current active mode
+let setIcon = (tabId) => {
+  let m = activeMode()
+  chrome.browserAction.setIcon({path: m.icon, tabId})
+  chrome.browserAction.setTitle({title: m.title, tabId})
+}
+
+
+// Load saved mode
+chrome.webNavigation.onCompleted.addListener((tab) => {
+  chrome.storage.local.get(State, (item) => {
+    State = item
+    setIcon(tab.id)
+  })
+}, {url: [{hostEquals : MAIN_HOST}]})
+
+
+// Rotate modes
+chrome.browserAction.onClicked.addListener((tab) => {
+  rotateMode()
+  setIcon(tab.id)
+  chrome.storage.local.set(State)
+})
+
+
+// Mark finished downloads
+chrome.downloads.onChanged.addListener((delta) => {
+  if (delta.state?.current != 'complete')
+    return
+
+  Downloads.remove(delta.url)
+})
+
+
+// Intercept the subtitles requests, and let the active mode handle it.
+chrome.webRequest.onBeforeRequest.addListener(
+  (req) => activeMode().handle(req),
+  {urls: [SUBS_URLS]},
+  ['blocking']
 )
